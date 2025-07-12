fd3a4ab5625bc83a48640b92801ddbca570d3a40
ffbrunoff1 <bruno@ntcbrasil.com.br>
2025-07-12 00:01:25 -0300

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+{
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5BBAE0',
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config
